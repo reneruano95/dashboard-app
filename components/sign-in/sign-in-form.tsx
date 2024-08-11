1ec5f31a44f2f7423dde3c295994f5e56ba0391d
Rene Ruano <rene.ruano95@gmail.com>
2024-08-11 19:59:01 -0400

    refactor: update createBrowserClient function to use useMemo for Supabase client initialization

--- components/sign-in/sign-in-form.tsx
+++ components/sign-in/sign-in-form.tsx
@@ -131,7 +131,6 @@ export const SignInForm = () => {
                 </div>
               )} */}
           </Button>
-          {/* <ModeToggle /> */}
         </form>
       </Form>
 
--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,9 +1,14 @@
+import { useMemo } from "react";
 import { createBrowserClient as createClient } from "@supabase/ssr";
 import { Database } from "../types/database.types";
 
 export function createBrowserClient() {
-  return createClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  const supabase = useMemo(() => {
+    return createClient<Database>(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    );
+  }, []);
+
+  return supabase;
 }
