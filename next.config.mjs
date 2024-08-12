bb029a3f400d0c7690fca647db698c5812033904
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-08-12 11:48:03 +0000

    chore: Add header validation in middleware to prevent header mismatch

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  crossOrigin: "anonymous",
+  poweredByHeader: false,
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
