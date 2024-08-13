d5e6a8cf4aa0eaee9b0cdcf4f283a480dd7aaf16
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-08-13 12:13:26 +0000

    refactor: Update user details fetching and error handling in DashboardPage

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,12 +1,9 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
-import { useGetUser } from "@/lib/hooks/useGetUser";
-import { createServerClient } from "@/lib/supabase/server";
+import { QueryClient } from "@tanstack/react-query";
 import { redirect } from "next/navigation";
+
+import { createServerClient } from "@/lib/supabase/server";
 import { getUserDetails } from "@/lib/actions/users";
+import { AgencyUser } from "@/lib/types";
 
 export default async function DashboardPage() {
   const queryClient = new QueryClient();
@@ -21,14 +18,27 @@ export default async function DashboardPage() {
     return redirect("/sign-in");
   }
 
-  const userDetails = await queryClient.fetchQuery({
-    queryKey: ["user", user.id],
-    queryFn: () => getUserDetails(user.id),
-    
-  });
+  let userDetails: AgencyUser | null = null;
+  try {
+    userDetails = await queryClient.fetchQuery({
+      queryKey: ["user", user.id],
+      queryFn: () => getUserDetails(user.id),
+    });
+  } catch (error) {
+    console.error(error);
+  }
 
   if (!userDetails) {
-    return redirect("/sign-in");
+    return (
+      <div className="h-screen flex flex-1 flex-col justify-center items-center">
+        <h1 className="text-2xl">
+          An error occurred while fetching user details.
+        </h1>
+        <p className="text-lg text-foreground/80">
+          Please contact support for further assistance.
+        </p>
+      </div>
+    );
   }
 
   return redirect(`/dashboard/${userDetails.agency_id}`);
