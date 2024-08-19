f6b7b5915476591c4bb6432d1e66d0c5a84c0785
Rene Ruano <rene.ruano95@gmail.com>
2024-08-18 20:01:20 -0400

    refactor: Update package.json and layout.tsx, add usehooks-ts dependency and sidebar component in agency layout

--- app/(protected)/[agency_id]/layout.tsx
+++ app/(protected)/[agency_id]/layout.tsx
@@ -9,6 +9,7 @@ export default function AgencyLayout({
     <>
       <main className="flex items-center justify-center h-screen">
         <Sidebar />
+
         {children}
       </main>
     </>
--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -1,46 +1,119 @@
-import Link from "next/link";
-import Image from "next/image";
+"use client";
+
+import { ChevronsLeft, MenuIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { ElementRef, useEffect, useRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+
+import { cn } from "@/lib/utils";
+
 import { Icon } from "../global/icon";
 
-import logoImage from "@/public/logo.80111550.webp";
-import { AspectRatio } from "../ui/aspect-ratio";
-
 export const Sidebar = () => {
+  const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+  const sidebarRef = useRef<ElementRef<"aside">>(null);
+  const navbarRef = useRef<ElementRef<"div">>(null);
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    } else {
+      resetWidth();
+    }
+  }, [isMobile]);
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    }
+  }, [pathname, isMobile]);
+
+  const resetWidth = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(false);
+      setIsResetting(true);
+
+      sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+      navbarRef.current.style.setProperty(
+        "width",
+        isMobile ? "0" : "calc(100% - 240px)"
+      );
+      navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
+      setTimeout(() => setIsResetting(false), 300);
+    }
+  };
+
+  const collapse = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(true);
+      setIsResetting(true);
+
+      sidebarRef.current.style.width = "0";
+      navbarRef.current.style.setProperty("width", "100%");
+      navbarRef.current.style.setProperty("left", "0");
+      setTimeout(() => setIsResetting(false), 300);
+    }
+  };
+
   return (
-    <aside className="hidden h-full w-64 bg-white border-r border-gray-200 md:block dark:bg-background dark:border-neutral-700">
-      <div className="relative flex flex-col h-full max-h-full">
-        <div className=" px-6 mx-3 mt-3 dark:bg-white rounded-lg">
-          <AspectRatio ratio={16 / 5}>
-            <Image
-              src={logoImage}
-              alt="Logo"
-              className="object-contain w-full h-full rounded-lg dark:filter"
-              quality={100}
+    <>
+      <aside
+        ref={sidebarRef}
+        className={cn(
+          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "w-0"
+        )}
+      >
+        <div
+          onClick={collapse}
+          role="button"
+          className={cn(
+            "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
+            isMobile && "opacity-100"
+          )}
+        >
+          <ChevronsLeft className="h-6 w-6" />
+        </div>
+
+        <div className="flex flex-col">
+          <div>
+            <Icon name="Search" className="w-5 h-5" />
+          </div>
+          <div>
+            <Icon name="Settings" className="w-5 h-5" />
+          </div>
+        </div>
+
+        <div className="mt-4">Add Page</div>
+        <div
+          onClick={resetWidth}
+          className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
+        />
+      </aside>
+
+      <div
+        ref={navbarRef}
+        className={cn(
+          "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "left-0 w-full"
+        )}
+      >
+        <nav className="bg-transparent px-3 py-2 w-full">
+          {isCollapsed && (
+            <MenuIcon
+              onClick={resetWidth}
+              role="button"
+              className="h-6 w-6 text-muted-foreground"
             />
-          </AspectRatio>
-        </div>
-
-        <div className="h-full overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
-          <nav className="p-3 w-full flex flex-col flex-wrap">
-            <ul className="flex flex-col space-y-1">
-              <li>
-                <Link
-                  className="flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:bg-neutral-700 dark:text-neutral-100 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600"
-                  href="#"
-                >
-                  <Icon name="LayoutDashboard" className="w-5 h-5" />
-                  Dashboard
-                </Link>
-              </li>
-
-              <li className="flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:bg-neutral-700 dark:text-neutral-100 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 hover:cursor-pointer">
-                <Icon name="Settings" className="w-5 h-5" />
-                Settings
-              </li>
-            </ul>
-          </nav>
-        </div>
+          )}
+        </nav>
       </div>
-    </aside>
+    </>
   );
 };
