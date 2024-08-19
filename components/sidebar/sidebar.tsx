9a42164508bcb4855c6a6fc61f5710c0c532d560
Rene Ruano <rene.ruano95@gmail.com>
2024-08-19 18:45:03 -0400

    feat(sidebar): add pages & app sections

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -7,6 +7,7 @@ import { useMediaQuery } from "usehooks-ts";
 
 import { Icon } from "../global/icon";
 import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
 
 export const Sidebar = () => {
   const pathname = usePathname();
@@ -81,16 +82,91 @@ export const Sidebar = () => {
           </div>
         </div>
 
-        <ul className="flex flex-col p-2 gap-[1px]">
+        <ul className="flex flex-col p-2 gap-[1px] border-b">
           <li className="group/native flex items-center gap-2 transition-all cursor-pointer text-muted-foreground py-1 px-2 hover:bg-neutral-300 dark:hover:bg-neutral-600 rounded-sm">
             <Icon name="Search" className="w-5 h-5" />
             <span>Search</span>
+          </li>
+          <li className="group/native flex items-center gap-2 transition-all cursor-pointer text-muted-foreground py-1 px-2 hover:bg-neutral-300 dark:hover:bg-neutral-600 rounded-sm">
+            <Icon name="House" className="w-5 h-5" />
+            <span>Home</span>
+          </li>
+          <li className="group/native flex items-center gap-2 transition-all cursor-pointer text-muted-foreground py-1 px-2 hover:bg-neutral-300 dark:hover:bg-neutral-600 rounded-sm">
+            <Icon name="Inbox" className="w-5 h-5" />
+            <span>Inbox</span>
           </li>
           <li className="group/native flex items-center gap-2 transition-all cursor-pointer text-muted-foreground py-1 px-2 hover:bg-neutral-300 dark:hover:bg-neutral-600 rounded-sm">
             <Icon name="Settings" className="w-5 h-5" />
             <span>Settings</span>
           </li>
         </ul>
+
+        <div className="px-2 mt-2">
+          <h3 className="mb-1 text-xs font-semibold text-zinc-500">Pages</h3>
+          <nav className="gap-1">
+            <Button
+              variant="ghost"
+              className="w-full justify-start py-1 px-2 h-fit hover:bg-neutral-300 dark:hover:bg-neutral-600 text-muted-foreground"
+            >
+              <Icon
+                name="NotebookTabs"
+                className="mr-2 h-5 w-5 text-green-500"
+              />
+              <span className="text-base">Estimates</span>
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-full justify-start py-1 px-2 h-fit hover:bg-neutral-300 dark:hover:bg-neutral-600 text-muted-foreground"
+            >
+              <Icon
+                name="NotebookTabs"
+                className="mr-2 h-5 w-5 text-green-500"
+              />
+              <span className="text-base">Invoices</span>
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-full justify-start py-1 px-2 h-fit hover:bg-neutral-300 dark:hover:bg-neutral-600 text-muted-foreground"
+            >
+              <Icon name="Users" className="mr-2 h-5 w-5 text-green-500" />
+              <span className="text-base">Customers</span>
+            </Button>
+          </nav>
+        </div>
+
+        <div className="px-2 mt-2">
+          <h3 className="mb-1 text-xs font-semibold text-zinc-500">Apps</h3>
+          <nav className="gap-1">
+            <Button
+              variant="ghost"
+              className="w-full justify-start py-1 px-2 h-fit hover:bg-neutral-300 dark:hover:bg-neutral-600 text-muted-foreground"
+            >
+              <Icon name="ListTodo" className="mr-2 h-5 w-5 text-green-500" />
+              <span className="text-base">To-dos</span>
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-full justify-start py-1 px-2 h-fit hover:bg-neutral-300 dark:hover:bg-neutral-600 text-muted-foreground"
+            >
+              <Icon name="Kanban" className="mr-2 h-5 w-5 text-green-500" />
+              <span className="text-base">KanBan</span>
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-full justify-start py-1 px-2 h-fit hover:bg-neutral-300 dark:hover:bg-neutral-600 text-muted-foreground"
+            >
+              <Icon name="Workflow" className="mr-2 h-5 w-5 text-green-500" />
+              <span className="text-base">Automation</span>
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-full justify-start py-1 px-2 h-fit hover:bg-neutral-300 dark:hover:bg-neutral-600 text-muted-foreground"
+            >
+              <Icon name="Calendar" className="mr-2 h-5 w-5 text-green-500" />
+              <span className="text-base">Calendar</span>
+            </Button>
+          </nav>
+        </div>
 
         <div className="opacity-0 group-hover/sidebar:opacity-100 transition absolute h-full w-1 bg-primary/10 right-0 top-0" />
       </aside>
