068c16cedeb778c9b1435f5c06ad064c25293944
Rene Ruano <rene.ruano95@gmail.com>
2024-08-20 18:44:30 -0400

    refactor: Separate dashboard layout logic into async function

---
+++ app/(protected)/dashboard/(main)/page.tsx
@@ -0,0 +1,11 @@
+export default async function DashboardPage() {
+  return (
+    <div className="flex flex-col flex-1 justify-center items-center h-screen">
+      <h1 className="text-2xl">Dashboard</h1>
+      <p className="text-lg">
+        Welcome to the admin dashboard! This is where you can manage all the
+        agencies and users in your organization.
+      </p>
+    </div>
+  );
+}
