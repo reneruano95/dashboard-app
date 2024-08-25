5fc8e7d7c8701fe1ac2611951745ee17b9c9f204
Rene Ruano <rene.ruano95@gmail.com>
2024-08-25 15:29:57 -0400

    refactor: optimize SidebarPagesItems component for improved readability and performance

---
+++ app/(protected)/dashboard/products/page.tsx
@@ -0,0 +1,12 @@
+export default function ProductsPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full space-y-4">
+      <h1 className="text-4xl font-bold text-center text-foreground/90">
+        Products Admin Page
+      </h1>
+      <p className="text-center text-lg text-foreground/80">
+        This is the Products Admin page.
+      </p>
+    </div>
+  );
+}
--- components/sidebar/sidebar-pages-items.tsx
+++ components/sidebar/sidebar-pages-items.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Fragment } from "react";
 import { usePathname } from "next/navigation";
 
 import { Icon } from "../global/icon";
@@ -6,55 +7,65 @@ import { Button } from "../ui/button";
 import { sidebarPages } from "@/lib/constants";
 import { cn } from "@/lib/utils";
 import { useAuth } from "@/lib/hooks/use-auth";
+import { Skeleton } from "../ui/skeleton";
 
 export const SidebarPagesItems = () => {
   const pathname = usePathname();
 
   const {
-    userRole: { data: role },
+    userRole: { data: role, isLoading, isFetching },
   } = useAuth();
 
-  const agencyId = pathname.split("/")[1];
+  const path = pathname.split("/")[1];
+
+  if (isLoading || isFetching) {
+    return (
+      <div className="px-2 mt-2 h-48">
+        <Skeleton className="w-full h-full rounded-sm bg-neutral-300 dark:bg-neutral-600" />
+      </div>
+    );
+  }
 
   return (
     <>
-      {sidebarPages.map(({ label, children }) => (
-        <div className="px-2 mt-2" key={label}>
-          <h3 className="mb-1 text-xs font-semibold text-zinc-500">{label}</h3>
-          {children.map(({ title, icon, href, roles }) => {
-            const isActive = pathname === `/${agencyId}${href}`;
-            const hasAccess = roles.includes(role!);
+      <div className="px-2 mt-2">
+        {sidebarPages.map(({ label, children }) => (
+          <Fragment key={label}>
+            <h3 className="mb-1 text-xs font-semibold text-zinc-500">
+              {label}
+            </h3>
+            {children.map(({ title, icon, href, roles }) => {
+              const isActive = pathname === `/${path}${href}`;
+              const hasAccess = roles.includes(role!);
 
-            if (!hasAccess) {
-              return null;
-            }
+              if (!hasAccess) return null;
 
-            return (
-              <Button
-                key={title}
-                variant="ghost"
-                className={cn(
-                  "w-full justify-start py-1 px-2 h-fit hover:bg-neutral-300 dark:hover:bg-neutral-600 text-muted-foreground",
-
-                  !hasAccess && "cursor-not-allowed"
-                )}
-                asChild
-              >
-                <Link
-                  href={`/${agencyId}${href}`}
+              return (
+                <Button
+                  key={title}
+                  variant="ghost"
                   className={cn(
-                    "flex items-center",
-                    isActive && "bg-neutral-300 dark:bg-neutral-600"
+                    "w-full justify-start py-1 px-2 h-fit hover:bg-neutral-300 dark:hover:bg-neutral-600 text-muted-foreground",
+                    !hasAccess && "cursor-not-allowed"
                   )}
+                  asChild
                 >
-                  <Icon name={icon} className="mr-2 h-5 w-5 text-green-500" />
-                  <span className="text-base">{title}</span>
-                </Link>
-              </Button>
-            );
-          })}
-        </div>
-      ))}
+                  <Link
+                    href={`/${path}${href}`}
+                    className={cn(
+                      "flex items-center",
+                      isActive && "bg-neutral-300 dark:bg-neutral-600"
+                    )}
+                  >
+                    <Icon name={icon} className="mr-2 h-5 w-5 text-green-500" />
+                    <span className="text-base">{title}</span>
+                  </Link>
+                </Button>
+              );
+            })}
+          </Fragment>
+        ))}
+      </div>
     </>
   );
 };
