aca839f9722f1270410f817fcaabca742801b6ea
Rene Ruano <rene.ruano95@gmail.com>
2024-08-29 22:13:25 -0400

    refactor: update TabMenuHorizontal and TabMenuVertical components to use consistent styling for active state

--- components/settings/tab-menu-horizontal.tsx
+++ components/settings/tab-menu-horizontal.tsx
@@ -6,17 +6,19 @@ import { ScrollArea } from "../ui/scroll-area";
 export const TabMenuHorizontal = () => {
   return (
     <Tabs defaultValue="My account" className="md:hidden">
-      <TabsList className="m-4 mb-2 flex h-fit">
-        {menuItemsAccount.map(({ labelMobile, label }) => (
-          <TabsTrigger
-            key={labelMobile}
-            value={label}
-            className="h-fit flex text-sm py-1 px-2"
-          >
-            {labelMobile}
-          </TabsTrigger>
-        ))}
-      </TabsList>
+      <div className="bg-muted px-3 py-2 flex justify-start">
+        <TabsList className="flex h-fit gap-[1px] bg-transparent p-0">
+          {menuItemsAccount.map(({ labelMobile, label }) => (
+            <TabsTrigger
+              key={labelMobile}
+              value={label}
+              className="h-fit flex text-sm py-1 px-2 hover:bg-neutral-300 dark:hover:bg-neutral-600 data-[state=active]:bg-neutral-300 dark:data-[state=active]:bg-neutral-600"
+            >
+              {labelMobile}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+      </div>
 
       <ScrollArea className="h-[calc(100vh-4rem)]">
         <TabMenuContent />
--- components/settings/tab-menu-vertical.tsx
+++ components/settings/tab-menu-vertical.tsx
@@ -53,7 +53,8 @@ export const TabMenuVertical = () => {
               <TabsTrigger
                 key={label}
                 value={label}
-                className="w-full h-fit flex gap-2 justify-start text-sm py-1 px-3 hover:bg-neutral-300 dark:hover:bg-neutral-600"
+                className="w-full h-fit flex gap-2 justify-start text-sm py-1 px-3 hover:bg-neutral-300 dark:hover:bg-neutral-600
+                data-[state=active]:bg-neutral-300 dark:data-[state=active]:bg-neutral-600"
               >
                 <Icon name={icon} className="w-5 h-5" />
                 <span>{label}</span>
